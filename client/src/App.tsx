186d5ee6d25cd791e23b001eccb466558b313dac
Jacobo Jaramillo <Extern.Jacobo.Molina@kuka.com>
2021-10-28 18:29:18 -0500

    FE. arreglar estilos.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
+import classes from './App.module.scss';
 import Footer from './components/UI/Layout/Footer'
 import Header from './components/UI/Layout/Header'
 
 function App(): JSX.Element {
 	return (
-		<>
-			<Header/>
-			<Footer/>
-		</>
+		<div className={classes.wrapper}>
+			<Header />
+			<div className={classes.content}></div>
+			<Footer />
+		</div>
 	);
 }
 

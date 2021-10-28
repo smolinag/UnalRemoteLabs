39291acea73f54ae03a2601b0209f928cf535a81
Daniel Molina <dmolinag@unal.edu.co>
2021-10-28 14:58:19 -0500

    FE codigo organizado

--- client/src/components/UI/Layout/Header.tsx
+++ client/src/components/UI/Layout/Header.tsx
@@ -1,43 +1,40 @@
 import React from 'react';
-import { Navbar, Nav, Image } from 'react-bootstrap';
-import { BiExit } from "react-icons/bi";
-import { BsPersonCircle } from "react-icons/bs";
+import {Navbar, Nav, Image} from 'react-bootstrap';
+import {BiExit} from 'react-icons/bi';
+import {BsPersonCircle} from 'react-icons/bs';
 
 import logosimbolo from '../../../images/logosimbolo.png';
 import classes from './Header.module.scss';
 
-// import './Header.module.scss'
-
 const Header: React.FC<unknown> = () => {
-   return (
-      <header>
-         <Navbar expand="lg" className={classes.navbar}>
-            <Navbar.Brand href="#home">
-               <Image src={logosimbolo} className={classes['navbar-brand']} />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav" >
-               <Nav className={classes.title}>
-                  <Navbar.Text >
-                     <h4>Laboratorios remotos Universidad Nacional de Colombia</h4>
-                  </Navbar.Text>
-               </Nav>
-               <Nav className={classes.optionNavbar}>
-                  <div className={classes.option}>
-                     <BsPersonCircle className={classes.icon} />
-                     <span>Daniel Molina</span>
-                  </div>
+	return (
+		<header>
+			<Navbar expand="lg" className={classes.navbar}>
+				<Navbar.Brand href="#home">
+					<Image src={logosimbolo} className={classes['navbar-brand']} />
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className={classes.title}>
+						<Navbar.Text>
+							<h4>Laboratorios remotos Universidad Nacional de Colombia</h4>
+						</Navbar.Text>
+					</Nav>
+					<Nav className={classes.optionNavbar}>
+						<div className={classes.option}>
+							<BsPersonCircle className={classes.icon} />
+							<span>Daniel Molina</span>
+						</div>
 
-                  <div className={classes.option}>
-                     <BiExit className={classes.icon} />
-                     <span>Salir</span>
-                  </div>
-
-               </Nav>
-            </Navbar.Collapse>
-         </Navbar>
-      </header>
-   )
+						<div className={classes.option}>
+							<BiExit className={classes.icon} />
+							<span>Salir</span>
+						</div>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		</header>
+	);
 };
 
 export default Header;

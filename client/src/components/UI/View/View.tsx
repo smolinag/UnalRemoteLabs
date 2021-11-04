4a29be772184192041fda811165b6f95c3b8ed22
Daniel Molina <dmolinag@unal.edu.co>
2021-11-04 18:45:44 -0500

    FE: Query para traer la inormación del laboratorio: nombre, duración

--- client/src/components/UI/View/View.tsx
+++ client/src/components/UI/View/View.tsx
@@ -2,68 +2,71 @@ import React from 'react';
 import {Row, Col} from 'react-bootstrap';
 
 import data2 from '../../../data/data.json';
-import {useGetPracticeInfoQuery} from '../../../graphql/generated/schema'
+import {useGetPracticeInfoQuery} from '../../../graphql/generated/schema';
 import {Switch, Table} from '../index';
 import classes from './View.module.scss';
 
 interface commandListDto {
-	"name": string, 
-	"parameter": string, 
-	"type": string, 
-	"value": boolean
+	name: string;
+	parameter: string;
+	type: string;
+	value: boolean;
 }
 
 const View: React.FC<unknown> = () => {
-	const [commandList, setCommandList] = React.useState<commandListDto[]>([])
+	const [commandList, setCommandList] = React.useState<commandListDto[]>([]);
 	const {data} = useGetPracticeInfoQuery();
 	// const {data: commands } = useGetLabCommandsListkQuery();
-	
+
 	React.useEffect(() => {
-		setCommandList(data2[0].commandlist)
-		console.warn(data)
+		setCommandList(data2[0].commandlist);
+		console.warn(data);
 	}, [data]);
 
 	const test = () => {
 		console.warn(test);
 	};
 
-   const columns: string[] = ["Parámetros", "Valores"]
+	const columns: string[] = ['Parámetros', 'Valores'];
 
 	return (
 		<>
 			<Row className={classes.section}>
 				<h3 className={classes.title}>{data?.getLabPractice?.name}</h3>
 				<span>Descripción: {data?.getLabPractice?.description}</span>
-				<span>Duración: {data?.getLabPractice?.duration ? data?.getLabPractice?.duration : "-"} segundos</span>
+				<span>Duración: {data?.getLabPractice?.duration ? data?.getLabPractice?.duration : '-'} segundos</span>
 			</Row>
 			<Row className={classes.section}>
 				<h4 className={classes.title}>Comandos de entrada</h4>
 				<Row>
-					<div className={classes.commands}>
-						{commandList.map((command) => {
-							return <div key={command.parameter} className={classes.command}>
-								<Switch label={command.name} state={command.value} onToggle={test}/>
-								</div>
-						})}
-					</div>
+					<Col>
+						<div className={classes.commands}>
+							{commandList.map((command) => {
+								return (
+									<div key={command.parameter} className={classes.command}>
+										<Switch label={command.name} state={command.value} onToggle={test} />
+									</div>
+								);
+							})}
+						</div>
+					</Col>
+					<Col>
+						<p>Histórico de comandos</p>
+						<Table headers={columns} data={data2[0].data} />
+					</Col>
 				</Row>
 			</Row>
 			<Row className={classes.section}>
 				<h4 className={classes.title}>Párametros de salida</h4>
 				<Row>
-               {
-                  data2[0].imagenReal && (
-                     <Col md={6}>
-                        <h5>Imagen tiempo real</h5>
-                     </Col>
-                  )
-               }
+					{data2[0].imagenReal && (
+						<Col md={6}>
+							<h5>Imagen tiempo real</h5>
+						</Col>
+					)}
 					<Col md={6}>
 						<h5>Datos</h5>
-                  <Table
-                     headers={columns}
-                     data={data2[0].data}
-                  />
+						<Table headers={columns} data={data2[0].data} />
 					</Col>
 				</Row>
 			</Row>

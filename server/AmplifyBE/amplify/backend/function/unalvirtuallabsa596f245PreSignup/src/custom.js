232bcc0e492d45599b4d4e04e7ddb3dc4e827376
Juan Andres <Juan Andres>
2022-02-08 16:27:19 -0500

    Cognito triggers implemented
    - Create user in database after creation in cognito.
    - Add User in Students group after sign up.
    - Limit the sign up according to email domain

--- server/AmplifyBE/amplify/backend/function/unalvirtuallabsa596f245PreSignup/src/custom.js
+++ server/AmplifyBE/amplify/backend/function/unalvirtuallabsa596f245PreSignup/src/custom.js
@@ -1,7 +1,16 @@
-exports.handler = (event, context, callback) => {
-  // insert code to be executed by your lambda trigger
-  console.log(JSON.stringify(event));
-  console.log(JSON.stringify(context));
-  console.log(JSON.stringify(callback));
+exports.handler = async (event) => {
+  // allowed domains
+  const DOMAIN_ALLOW_LIST = ["unal.edu.co", "gmail.com"];
+
+  const { email } = event.request.userAttributes;
+  const domain = email.substring(email.indexOf("@") + 1);
+
+  if (!DOMAIN_ALLOW_LIST.includes(domain)) {
+    console.log(`Invalid email domain: ${domain}`);
+    throw new Error(`Invalid email domain: ${domain}`);
+  } else {
+    console.log(`Valid email domain: ${domain}`);
+  }
+
   return event;
 };

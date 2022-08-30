6eec3714fd0cc51789cbae993d45449a020e9b62
samogir <santiago.molina.g@gmail.com>
2022-08-30 11:26:57 -0500

    Adjusted Iot-to-appsync Lambda Function to consider filter the labPracticeOutputs by the provided deviceId

--- server/ServerlessBE/index.js
+++ server/ServerlessBE/index.js
@@ -27,6 +27,17 @@ const client = new ApolloClient({
 });
 
 //---GQL Queries---
+const LIST_LAB_PRACTICES = gql`
+  query listLabPractices($filter: ModelLabPracticeFilterInput) {
+    listLabPractices(filter: $filter) {
+      items {
+        id
+        name
+      }
+    }
+  }
+`;
+
 const LIST_OUTPUTS = gql`
   query listLabPracticeOutputs($filter: ModelLabPracticeOutputFilterInput) {
     listLabPracticeOutputs(filter: $filter) {
@@ -79,6 +90,32 @@ const UPDATE_COMMAND = gql`
 `;
 //---------------
 
+async function listLabPractices(filter) {
+  try{
+    //Query labPractice to get id
+    const queryAns = await client.query({
+      query: LIST_LAB_PRACTICES,
+      variables: { filter },
+    });
+    let practices = queryAns.data.listLabPractices.items;
+    if(practices.length > 0) {
+      console.log("LabPractices found before deleted filter: " + practices.length)
+      practices = practices.filter(item => item._deleted !== true);
+      if(practices.length > 0) {
+        console.log("LabPractice found: " + practices[0].id);
+        return practices[0].id;
+      } else {
+        console.log("LabPractice not found by filter deleted")
+      }      
+    } else {
+      console.log("LabPractice not found by filter: " + filter)
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
+}
+
 async function listLabPracticeOutputs(filter) {
   try {
     //Query labPracticeOutput to get id
@@ -88,9 +125,14 @@ async function listLabPracticeOutputs(filter) {
     });
     let outputs = queryAns.data.listLabPracticeOutputs.items;
     if (outputs.length > 0) {
-      console.log("Output id: " + outputs[0].id);
-      outputs = outputs.filter(item => item._deleted != true);
-      return outputs[0].id;
+      console.log("Outputs before deleted filter: " + outputs.length);
+      outputs = outputs.filter(item => item._deleted !== true);
+      if(outputs.length > 0) {
+        console.log("Output found: " + outputs[0].id)
+        return outputs[0].id;
+      } else {
+        console.log("Output not found by filter deleted")
+      }      
     } else {
       console.log("Output not found");
     }
@@ -150,60 +192,81 @@ async function labOutputListen(rpiId, outputId, value, captureDate) {
 }
 
 exports.handler = async (event) => {
-  console.log(event);
-  switch (event.type) {
-    case "output":
-      for (let i = 0; i < event.data.length; i++) {
-        let item = event.data[i];
-        console.log(item);
-        let outputId = await listLabPracticeOutputs({ name: { eq: item.name } });
-        await labOutputListen(event.rpiId, outputId, item.value, event.captureDate);
+  console.log(event);  
+  if(event.rpiId){
+    let labPracticeId = await listLabPractices({ LabPracticeDeviceId: { eq: event.rpiId } });
+    if(labPracticeId){
+      switch (event.type) {
+        case "output":
+          for (let i = 0; i < event.data.length; i++) {
+            let item = event.data[i];
+            console.log(item);        
+            let outputId = await listLabPracticeOutputs({ name: { eq: item.name }, labpracticeID: {eq: labPracticeId} });
+            await labOutputListen(event.rpiId, outputId, item.value, event.captureDate);
+          }
+          break;
+        case "command":
+          await updateLabPracticeSessionCommand(event.uuid, event.status, event.executionDate);
+          break;
+        default:
+          console.log("Event type not valid: " + event.type);
+          break;
       }
-      break;
-    case "command":
-      await updateLabPracticeSessionCommand(event.uuid, event.status, event.executionDate);
-      break;
-    default:
-      console.log("Event type not valid: " + event.type);
-      break;
-  }
+    } else {
+      console.log("No LabPractice found for DeviceId: " + event.rpiId)
+    }    
+  } else {
+    console.log("No DeviceId provided...")
+  }  
 };
 
 // main();
 
 // async function main() {
-//   // let event = {
-//   //   sessionId: "aaaaaaa",
-//   //   type: "output",
-//   //   data: [
-//   //     { name: "Plot_V_Fase1", value: { x: [1, 2, 3, 4], y: [11, 12, 13, 11], xAxisUnits: "ms", yAxisUnits: "A" }, status: "ok" },
-//   //     { name: "V_Fase2", value: "120VAC", status: "ok" },
-//   //     { name: "V_Fase3", value: "121VAC", status: "ok" },
-//   //     { name: "I_Fase1", value: "0.87A", status: "ok" },
-//   //     { name: "I_Fase2", value: "0.4A", status: "ok" },
-//   //     { name: "I_Fase3", value: "No se puede abrir el puerto COM4", status: "error" },
-//   //   ],
-//   //   captureDate: "2021-11-09T16:40:00-05:00",
-//   // };
 //   let event = {
-//     type: "command",
-//     uuid: "79fa13d2-1f0f-41d6-b465-6031de748aa7",
-//     status: "Hi",
-//     executionDate: "2021-11-25T16:39:06.008553-05:00",
+//     rpiId: "7baec697-ddfa-411a-b9e9-0eab42cbeb35",
+//     type: "output",
+//     data: [
+//       { name: "V_Fase1", value: { x: [1, 2, 3, 4], y: [11, 12, 13, 11], xAxisUnits: "ms", yAxisUnits: "A" }, status: "ok" },
+//       { name: "V_Fase2", value: "120VAC", status: "ok" },
+//       { name: "V_Fase3", value: "121VAC", status: "ok" },
+//       { name: "I_Fase1", value: "0.87A", status: "ok" },
+//       { name: "I_Fase2", value: "0.4A", status: "ok" },
+//       { name: "I_Fase3", value: "No se puede abrir el puerto COM4", status: "error" },
+//     ],
+//     captureDate: "2021-11-09T16:40:00-05:00",
 //   };
+//   // let event = {
+//   //   type: "command",
+//   //   uuid: "79fa13d2-1f0f-41d6-b465-6031de748aa7",
+//   //   status: "Hi",
+//   //   executionDate: "2021-11-25T16:39:06.008553-05:00",
+//   // };
 
-//   switch (event.type) {
-//     case "output":
-//       event.data.forEach(async (item) => {
-//         let outputId = await listLabPracticeOutputs({ name: { eq: item.name } });
-//         labOutputListen(event.sessionId, outputId, item.value, event.captureDate);
-//       });
-//       break;
-//     case "command":
-//       updateLabPracticeSessionCommand(event.uuid, event.status, event.executionDate);
-//       break;
-//     default:
-//       console.log("Event type not valid: " + event.type);
-//       break;
-//   }
+// console.log(event);  
+//   if(event.rpiId){
+//     let labPracticeId = await listLabPractices({ LabPracticeDeviceId: { eq: event.rpiId } });
+//     if(labPracticeId){
+//       switch (event.type) {
+//         case "output":
+//           for (let i = 0; i < event.data.length; i++) {
+//             let item = event.data[i];
+//             console.log(item);        
+//             let outputId = await listLabPracticeOutputs({ name: { eq: item.name }, labpracticeID: {eq: labPracticeId} });
+//             await labOutputListen(event.rpiId, outputId, item.value, event.captureDate);
+//           }
+//           break;
+//         case "command":
+//           await updateLabPracticeSessionCommand(event.uuid, event.status, event.executionDate);
+//           break;
+//         default:
+//           console.log("Event type not valid: " + event.type);
+//           break;
+//       }
+//     } else {
+//       console.log("No LabPractice found for DeviceId: " + event.rpiId)
+//     }    
+//   } else {
+//     console.log("No DeviceId provided...")
+//   }  
 // }
